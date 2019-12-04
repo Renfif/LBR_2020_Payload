416a73ae83dd160b95f2fbb4629a9da19b8e337d
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-12-04 00:25:24 -0800

    Create gart.c

---
+++ Arduino/UAV/Memes/gart.c
@@ -0,0 +1,48 @@
+
+int receiverCh1=3; // for fly sky
+int Ch1=0; // tracks receiver values as it is read
+int R_en=7; // for hbridge
+int L_en=8; // for hbridge
+int RPWM=5; // for hbridge
+int LPWM=6;// for hbridge
+
+void setup() 
+{
+  pinMode(receiverCh1,INPUT);
+  pinMode(R_en, OUTPUT);
+  pinMode(L_en, OUTPUT);
+  pinMode(RPWM, OUTPUT);
+  pinMode(LPWM, OUTPUT);
+
+  Serial.begin(9600);
+ }
+
+
+void loop() {
+    digitalWrite(R_en, HIGH);
+    digitalWrite(L_en, HIGH);
+    Ch1=pulseIn(receiverCh1,HIGH);
+    
+    //Serial.println("fuck you");
+    Serial.println(Ch1);
+
+    
+  //  FlySky controller transmitting data
+    if (Ch1 >= 900 && Ch1 <=1100) //switch on transmitter is upward position= FORWARD
+    { 
+        digitalWrite(RPWM, LOW); 
+        digitalWrite(LPWM, HIGH);    
+    }
+    if (Ch1 >= 1400 && Ch1 <=1500) //switch on transmitter is in middle position= STOP 
+    { 
+        digitalWrite(RPWM, LOW);
+        digitalWrite(LPWM, LOW);
+    }
+    if (Ch1 >=1900 && Ch1 <= 2000) // switch on transmitter is in downward position= BACKWARDS
+    { 
+        digitalWrite(RPWM, HIGH);
+        digitalWrite(LPWM, LOW);
+        
+    }
+
+}
